35ade780eb0d4a1989acfe4ce36f6a9a5a6272ac
sagarpatil860 <sagarpatil860@gmail.com>
2021-12-12 00:17:25 +0530

    second commit

--- mdn_to_do_list/src/index.js
+++ mdn_to_do_list/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App tasks={DATA}/>
+  ,
   document.getElementById('root')
 );
 
